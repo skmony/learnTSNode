8cdbc4e494e99db131f018ef19803f6f5943dab8
Shashikant Mony <shashikantmony@gmail.com>
2020-05-28 02:34:58 +0530

    Initial Commit

---
+++ src/routes/todos.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  createTodo,
+  getTodo,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todo";
+const router = Router();
+
+router.post("/", createTodo);
+
+router.get("/", getTodo);
+
+router.patch("/:id", updateTodo);
+
+router.delete("/:id", deleteTodo);
+
+export default router;
